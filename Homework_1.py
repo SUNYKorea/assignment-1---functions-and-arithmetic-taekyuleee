2f86899f7d94729b7906fbefc3e6ac605197fbb5
taekyu lee <taekyu.lee@stonybrook.edu>
2023-04-13 10:21:35 +0900

    I modified exercise 1

--- Homework_1.py
+++ Homework_1.py
@@ -8,7 +8,7 @@
 # TODO: Complete the implementation of fahrenheit2celsius () and what_to_wear(). 
 
 def fahrenheit2celsius(fahrenheit): 
-   ...ewewq
+   x = 1
 
 def what_to_wear(celsius):
    ...
