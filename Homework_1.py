2add67a56c64010f6a7c0405a86c3fcdb228b289
taekyu lee <taekyu.lee@stonybrook.edu>
2023-04-19 15:59:01 +0900

    completed

--- Homework_1.py
+++ Homework_1.py
@@ -8,7 +8,7 @@
 # TODO: Complete the implementation of fahrenheit2celsius () and what_to_wear(). 
 
 def fahrenheit2celsius(fahrenheit): 
-   calsius = 5/9 *(fahrenheit-32)
+   celsius = 5/9 *(fahrenheit-32)
    return celsius
 def what_to_wear(celsius):
    if celsius < -10:
@@ -27,29 +27,34 @@ def what_to_wear(celsius):
 # compute_triangle_perimeter from scratch  
 
 def shoelace_triangle_area(x1, y1, x2, y2, x3, y3):
-    ...
+    area = abs((x1*y2+x2*y3+x3*y1) - (x1*y3+x2*y1+x3*y2)) / 2
+    return area
 
 def euclidean_distance(x1, y1, x2, y2):
-    ...
+    distance= ((x1-x2)**2 + (y2-y1)**2)
+    return distance
 
 def compute_triangle_perimeter(x1, y1, x2, y2, x3, y3):
-    ...
-
-
+    side1=euclidean_distance(x1,y1,x2,y2)
+    side2=euclidean_distance(x2,y2,x3,y3)
+    side3=euclidean_distance(x3,y3,x1,y1)
+    perimeter= side1+side2+side3
+    return perimeter
 # ---------------------------- Exercise III -------------------------------------
 # ----------------- Compute the area of a regular polygon -----------------------
 # TODO: Fill the functions deg2rad, apothem  and polygon_area 
 
 
 def deg2rad(deg):
-    ...
+    return math.p1*deg/180
 
 def apothem(number_sides, length_side):
-   ...
+    return number_sides/(2*math.tan(math.pi/length_side))
 
 def polygon_area(number_sides, length_side):
-   ...
-
+    a=apothem(number_sides)*(length_side)
+    perimeter=number_sides*length_side
+    return 0.5*a*perimeter
 
 # ---------------------------- Test -------------------------------------
 # The following lines are for testing purposes, and will not be part of
